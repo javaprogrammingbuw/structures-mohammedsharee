1a858b0f892d3aabb28a16859a8e2c6ea126110d
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-18 20:35:13 +0100

    Update SwitchCaseMod.java

--- SwitchCaseMod.java
+++ SwitchCaseMod.java
@@ -3,14 +3,64 @@ import java.util.Scanner;
 
 public class SwitchCaseMod {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
+
+
+			
+			if (b>0 && b<10) {
+				System.out.println("Modulus is");	
+				int M = a%b;
+				System.out.println(M);
+				
+				switch (M) {
+				case 0:
+					System.out.println("Modulus is Zero");
+					break;
+				case 1:
+					System.out.println("Modulus is ONE");
+					break;
+				case 2:
+					System.out.println("Modulus is TWO");
+					break;	
+				case 3:
+					System.out.println("Modulus is THREE");
+					break;
+				case 4:
+					System.out.println("Modulus is FOUR");
+					break;
+				case 5:
+					System.out.println("Modulus is FIVE");
+					break;
+				case 6:
+					System.out.println("Modulus is SIX");
+					break;
+				case 7:
+					System.out.println("Modulus is SEVEN");
+					break;
+				case 8:
+					System.out.println("Modulus is EIGHT");
+					break;
+				case 9:
+					System.out.println("Modulus is NINE");
+					break;
+				case 10:
+					System.out.println("Modulus is Ten");
+					break;
+				default:
+			System.out.println("Modulus cannot be found,");
+
+				}
+		
+		}else {
+			System.out.println("Wrong Value");
+		}
+			
+	
 		
 		/*
 		 * Todo: First check if 0<b<10.If this condition does not hold, print a little
