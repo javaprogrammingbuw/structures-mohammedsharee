c3cdb25dcd3b95a14712d47c41a0eedf3d9c0b44
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-18 20:33:36 +0100

    Update SumMeanMinMax.java

--- SumMeanMinMax.java
+++ SumMeanMinMax.java
@@ -3,21 +3,44 @@ import java.util.Scanner;
 
 public class SumMeanMinMax {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the value of A");
 		int a = scan.nextInt();
+		System.out.println("Enter the value of B");
 		int b = scan.nextInt();
+		
+		
+		System.out.println("Enter the value of c");
 		int c = scan.nextInt();
 		
-		/*
-		 * Todo: Print the sum, mean, max Value and min Value of a, b and c.
-		 * */
+		double sum = a+b+c ;
+		System.out.println("Sum is " + sum);
 		
+		double mean = sum/3 ;
+		System.out.println("mean is " + mean);
+		
+		if (a<b && a<c) {
+			System.out.println("A is smaller than all");
+		}else if (b<a && b<c) {
+			System.out.println("b is smaller than all");
+		
+	   }else {
+		    System.out.println("C is smaller than all");
+	   }
+		
+		if (a>b && a>c) {
+			
+			System.out.println("A is greater than all");
+		}else if (b>a && b>c) {
+			
+			System.out.println("b is greater than all");
+		
+	   }else {
+		   
+		    System.out.println("C is greater than all");
+	   }
 
 	}
-
 }
