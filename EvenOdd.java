9a015f6077f3a11662281d48cd2efea6c309e1be
Mohammed Shareef <sharif.ms10@gmail.com>
2019-03-18 15:55:01 +0100

    even odd done

--- EvenOdd.java
+++ EvenOdd.java
@@ -7,6 +7,19 @@ public class EvenOdd {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Enter the number ");
+		int a = scan.nextInt();
+	
+		for(int i=0; i<a; i++) {
+			if(i%2 != 0) {
+				System.out.println(  i + " " );	
+			
+			}
+			
+			
+			
+		}
+		
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
